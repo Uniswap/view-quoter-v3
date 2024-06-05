27f44482841da82fd31c110465bf09dc632d7061
Alice Henshaw <henshawalice@gmail.com>
2024-06-05 15:54:43 -0300

    update create2 formula

--- contracts/libraries/PoolAddress.sol
+++ contracts/libraries/PoolAddress.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.7.6;
 
 /// @title Provides functions for deriving a pool address from the factory, tokens, and the fee
 library PoolAddress {
-    bytes32 internal constant POOL_INIT_CODE_HASH = 0xe34f199b19b2b4f47f68442619d555527d244f78a3297ea89325f843f87b8b54;
+    bytes32 internal constant POOL_INIT_CODE_HASH = 0x010013f177ea1fcbc4520f9a3ca7cd2d1d77959e05aa66484027cb38e712aeed;
 
     /// @notice The identifying key of the pool
     struct PoolKey {
@@ -33,10 +33,11 @@ library PoolAddress {
                 uint256(
                     keccak256(
                         abi.encodePacked(
-                            hex"ff",
-                            factory,
-                            keccak256(abi.encode(key.token0, key.token1, key.fee)),
-                            POOL_INIT_CODE_HASH
+                            bytes32(0x2020dba91b30cc0006188af794c2fb30dd8520db7e2c088b7fc7c103c00ca494), // keccak256("zksyncCreate2")
+                            bytes32(uint256(uint160(factory))), // sender
+                            keccak256(abi.encode(key.token0, key.token1, key.fee)), // salt
+                            POOL_INIT_CODE_HASH, // bytecode hash
+                            bytes32(0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470) // constructor input hash: keccak256("")
                         )
                     )
                 )
--- script/Quoter.s.sol
+++ script/Quoter.s.sol
@@ -1,7 +1,8 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.7.6;
 
-import {Script, console2} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol";
+import {console2} from "forge-std/console2.sol";
 import {Quoter} from "../contracts/Quoter.sol";
 import {ChainId} from "v3-periphery/contracts/libraries/ChainId.sol";
 
@@ -13,7 +14,9 @@ contract MyScript is Script {
         vm.startBroadcast(deployerPrivateKey);
 
         address factory = getFactoryAddress();
+        console2.log("factory address:", factory);
         Quoter quoter = new Quoter(factory);
+        console2.log("quoter address:", address(quoter));
 
         vm.stopBroadcast();
     }
@@ -24,36 +27,39 @@ contract MyScript is Script {
         // base chain
         if (chainId == uint256(8453)) {
             return 0x33128a8fC17869897dcE68Ed026d694621f6FDfD;
-        // celo chain
+            // celo chain
         } else if (chainId == uint256(42220)) {
             return 0xAfE208a311B21f13EF87E33A90049fC17A7acDEc;
-        // bsc chain
+            // bsc chain
         } else if (chainId == uint256(56)) {
             return 0xdB1d10011AD0Ff90774D0C6Bb92e5C5c8b4461F7;
-        // optimism sepolia chain
+            // optimism sepolia chain
         } else if (chainId == uint256(11155420)) {
             return 0x8CE191193D15ea94e11d327b4c7ad8bbE520f6aF;
-        // arbitrum sepolia chain
+            // arbitrum sepolia chain
         } else if (chainId == uint256(421614)) {
             return 0x248AB79Bbb9bC29bB72f7Cd42F17e054Fc40188e;
-        // sepolia chain
+            // sepolia chain
         } else if (chainId == uint256(11155111)) {
             return 0x0227628f3F023bb0B980b67D528571c95c6DaC1c;
-        // avalanche chain
+            // avalanche chain
         } else if (chainId == uint256(43114)) {
             return 0x740b1c1de25031C31FF4fC9A62f554A55cdC1baD;
-        // zora chain
+            // zora chain
         } else if (chainId == uint256(7777777)) {
             return 0x7145F8aeef1f6510E92164038E1B6F8cB2c42Cbb;
-        // zora sepolia chain
+            // zora sepolia chain
         } else if (chainId == uint256(999999999)) {
             return 0x4324A677D74764f46f33ED447964252441aA8Db6;
-        // rookstock chain
+            // rookstock chain
         } else if (chainId == uint256(30)) {
             return 0xaF37EC98A00FD63689CF3060BF3B6784E00caD82;
-        // blast chain
+            // blast chain
         } else if (chainId == uint256(81457)) {
             return 0x792edAdE80af5fC680d96a2eD80A44247D2Cf6Fd;
+            // zksync
+        } else if (chainId == uint256(324)) {
+            return 0x8FdA5a7a8dCA67BBcDd10F02Fa0649A937215422;
         } else {
             return 0x1F98431c8aD98523631AE4a59f267346ea31F984;
         }
